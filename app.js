0c19dbf8a0ac81892331a1c81ecaf4a6e8f4e52b
Abed <abedj2023@Gmail.com>
2023-02-25 17:25:35 -0600

    splitting routes to multiple files

--- app.js
+++ app.js
@@ -2,72 +2,22 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const postBank = require("./postBank");
-const timeAgo = require("node-time-ago");
-
+const postList = require("./views/postList");
+const postDetails = require("./views/postDetails");
 app.use(morgan("dev"));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
 
-  const html = `<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Wizard News</title>
-    <link rel="stylesheet" href="/style.css" />
-  </head>
-  <body>
-    <div class="news-list">
-      <header><img src="/logo.png"/>Wizard News</header>
-      ${posts
-        .map(
-          (post) => `
-        <div class='news-item'>
-          <p>
-            <span class="news-position">${post.id}. ▲</span>
-            <a href="/posts/${post.id}">${post.title}</a>
-            <small>(by ${post.name})</small>
-          </p>
-          <small class="news-info">
-            ${post.upvotes} upvotes | ${timeAgo(post.date)}
-          </small>
-          
-        </div>`
-        )
-        .join("")}
-    </div>
-  </body>
-</html>`;
-  res.send(html);
+  res.send(postList(posts));
 });
 
 app.get("/posts/:id", (req, res) => {
   const id = req.params.id;
   const post = postBank.find(id);
-  const html = `<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Wizard News</title>
-    <link rel="stylesheet" href="/style.css" />
-  </head>
-  <body>
-    <div class="news-list">
-      <header><img src="/logo.png"/>Wizard News</header>
-        <div class='news-item'>
-          <p>
-            ${post.title}
-            <small>(by ${post.name})</small>
-          </p>
-          <p>
-            ${post.content}
-          </p>
-        </div>
-    </div>
-  </body>
-</html>
-`;
   if (post.id) {
-    res.send(html);
+    res.send(postDetails(post));
   } else {
     res.send("Opos! 404 - Page Not Found");
   }
